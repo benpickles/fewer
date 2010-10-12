e083a853ca11148f84c8d63dcd52793d60107e1c
Martyn Loughran <me@mloughran.com>
2010-10-12 15:11:13 +0100

    Rescued minification errors by returning unminified source
    * Typically minification is only enabled in production
    * Should avoid breaking production at all costs

--- lib/fewer/engines/js.rb
+++ lib/fewer/engines/js.rb
@@ -13,7 +13,16 @@ module Fewer
 
       def read
         if options[:min]
-          ::Closure::Compiler.new.compress(super)
+          begin
+            ::Closure::Compiler.new.compress(super)
+          rescue LoadError
+            Fewer.logger.error "Javascript minification not available since the closure-compiler gem is not installed."
+            super
+          rescue StandardError => e
+            # Unfortunately errors do not all descend from Closure::Error
+            Fewer.logger.error "Exception raised in closure-compiler gem: #{e.class} (#{e.message}). Javascript returned without minification."
+            super
+          end
         else
           super
         end
