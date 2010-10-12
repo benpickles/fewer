84c935f424de59e82115aab363c9d376c2a2120f
Martyn Loughran <me@mloughran.com>
2010-10-12 15:09:33 +0100

    Add Fewer.logger, which used the rails logger if available

--- lib/fewer.rb
+++ lib/fewer.rb
@@ -1,3 +1,20 @@
+module Fewer
+  class << self
+    attr_writer :logger
+
+    def logger
+      @logger ||= begin
+        defined?(Rails) ? Rails.logger : begin
+          require 'logger'
+          log = Logger.new(STDOUT)
+          log.level = Logger::INFO
+          log
+        end
+      end
+    end
+  end
+end
+
 require 'fewer/app'
 require 'fewer/engines'
 require 'fewer/errors'
