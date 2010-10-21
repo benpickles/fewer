55904e67028a5914be442ae1f1590ceb1eb938ad
Ben Pickles <spideryoung@gmail.com>
2010-10-21 13:13:28 +0100

    Modify test so that it actually requires the call to compact.

--- test/serializer_test.rb
+++ test/serializer_test.rb
@@ -49,8 +49,9 @@ class SerializerTest < Test::Unit::TestCase
     assert_equal '302', encoded
   end
 
-  def test_encode_invalid_file
-    encoded = Fewer::Serializer.encode(fs, ['doesnt-exist.css'])
+  def test_encode_invalid_files
+    encoded = Fewer::Serializer.encode(fs, %w(0 1 2 3 4 5 6 7 8 9 a b c d e f
+      g h i j k l m n o p q r s t u v w x y z 10))
     assert_equal '', encoded
   end
 
